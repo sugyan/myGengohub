d3c4949e0b8f98f021aa05e1cb73b99b495b586b
sugyan <sugi1982@gmail.com>
2011-06-11 15:10:30 +0900

    github api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
  */
 
 var express = require('express');
+var GitHubApi = require('github').GitHubApi;
 
 var app = module.exports = express.createServer();
 
@@ -28,16 +29,56 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', function(req, res){
+app.get('/', function (req, res){
     res.render('index', {
         title: 'Express'
     });
 });
 
-app.get('/repositories/:id', function (req, res) {
+app.get('/repositories/:user/:repo', function (req, res) {
     res.render('repositories/show', {
         title: 'repository',
-        id: req.params.id
+        user: req.params.user,
+        repo: req.params.repo
+    });
+});
+
+app.get('/api/repository', function (req, res) {
+    var user = req.param('user');
+    var repo = req.param('repo');
+    if (! (user && repo)) {
+        res.send(400);
+        return;
+    }
+
+    var callback = function (err, data) {
+        res.send(data);
+    };
+    var github = new GitHubApi(true);
+
+    github.getCommitApi().getBranchCommits(user, repo, 'master', function (err, data) {
+        if (err) {
+            console.error(err);
+            res.send(err.status);
+            return;
+        }
+        if (data.length > 0) {
+            var sha = data[0].tree;
+            github.getObjectApi().showTree(user, repo, sha, function (err, data) {
+                if (err) {
+                    console.error(err);
+                    res.send(err.status);
+                    return;
+                }
+                var i;
+                // TODO 見つからなかったとき？
+                for (i = data.length; i--;) {
+                    if (data[i].name.match(/README/i)) {
+                        github.getObjectApi().getRawData(user, repo, data[i].sha, callback);
+                    }
+                }
+            });
+        }
     });
 });
 
--- public/javascripts/myGengohub.js
+++ public/javascripts/myGengohub.js
@@ -1,3 +1,17 @@
-$(function () {
-    console.log('hello');
-});
+var path = location.pathname;
+
+if (path.match(/^\/repositories\//)) {
+    $(function () {
+        var m = path.match(/^\/repositories\/(\w+)\/(\w+)/);
+        $.ajax({
+            url: '/api/repository',
+            data: {
+                user: m[1],
+                repo: m[2]
+            },
+            success: function (data) {
+                $('body').append($('<pre>').text(data));
+            }
+        });
+    });
+}
